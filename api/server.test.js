e542ace0ca9b214998fc0d8a178d2020a17826f6
ctscofield <ctscofield@gmail.com>
2021-07-23 16:16:03 -0600

    get endpoint

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,24 @@
 // Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
+const request = require('supertest')
+const db = require('../data/dbConfig')
+const server = require('./server')
+
+beforeAll(async () => {
+  await db.migrate.rollback()
+  await db.migrate.latest()
+}) //migrate
+afterAll(async () => {
+  await db.destroy()
+})
+
+describe('[GET] /api/jokes', () => {
+  test('gets err when hit w/out token', async () => {
+    const res = await request(server).get('/api/jokes')
+    expect(res.body).toMatchObject({"message": "token required"})
+    expect(res.body).toBeTruthy()
+  })
+  test('snapshot test', async () => {
+    const res = await request(server).get('/api/jokes')
+    expect(res.body).toMatchSnapshot()
+  })
 })
